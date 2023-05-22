6d464fce05b9be640a7222e8aa88d7de67cde42c
Acewurldx <119708405+Acewurldx@users.noreply.github.com>
2023-05-22 21:52:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['2347062610678', 'Alienbot', true],
+  ['2347088652502', 'Acewurldx', true], 
   [''] 
 ] //Number of owners
 
@@ -47,15 +47,15 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'SANDARU-YT'
+global.botname = 'AlienBotx'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
+global.packname = 'AlienBotx' 
+global.author = '@Acewurldx' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/ace.gfx_\n' 
+global.dygp = 'https://chat.whatsapp.com/BI634Exhkv2KYHo9ckW7Ss'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.fgyt = 'https://youtube.com/@acewurldx'
+global.fgpyp = 'https://youtube.com/@acewurldx'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 global.dbase = 'mongodb://mongo:q4bHb1TK9mOyuKFmp1oY@containers-us-west-56.railway.app:6244' //ADD YOUR MONGODB BY CHANGIMG IT
 
